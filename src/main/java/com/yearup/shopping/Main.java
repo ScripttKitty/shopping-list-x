f253329cb082a34847278d2e1acccfaa52a543ed
Annie Santamaria <smaria.annie@gmail.com>
2023-10-06 15:16:56 -0400

    New shopping list

---
+++ src/main/java/com/yearup/shopping/Main.java
@@ -0,0 +1,25 @@
+package com.yearup.shopping;
+
+public class Main {
+
+    public static void main(String[ ] args){
+
+        System.out.println(" Hello Human here is your list");
+
+
+        int numberOfItems = 5;
+
+        //printItemCount(5);
+        /*
+        System.out.println("You have " + numberOfItems + " items. "
+         */
+        System.out.println("Enjoy your list!");
+
+        public void printItemCount(int n) {
+
+            System.out.println("You have" + n + " items.");
+
+        }
+
+    }
+}
